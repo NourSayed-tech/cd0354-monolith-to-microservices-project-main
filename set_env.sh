f1c89fb31571f6c3c1cf748228a7fe7d33c6d2a1
NourSayed-tech <noursayed585@gmail.com>
2022-09-30 23:22:43 +0200

    new

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=finalprodev1
+export POSTGRES_PASSWORD=finalprodev1
+export POSTGRES_HOST=finalprodev1.ctrawloinsg2.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::final-pro-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
