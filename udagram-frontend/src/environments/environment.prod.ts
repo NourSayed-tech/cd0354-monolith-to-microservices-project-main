f8cb3ddde0ffb731a0d8b58c6c972c46d6d77293
NourSayed-tech <68778927+NourSayed-tech@users.noreply.github.com>
2022-10-04 04:04:41 +0200

    Update environment.prod.ts

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'https://a69970c3b53354f54bf190c6d701910d-144795105.us-east-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
